4fbd3e7ca0b6892425e9b59cbd39620da51615b7
zak <zakt@miway.co.za>
2022-10-18 15:16:29 +0200

    Added v-bind to  mainNavigation href Added menu  items to    mainNavigation

---
+++ src/components/MainNav.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="w-full text-sm">
+    <div class="fixed top-0 left-0 w-full h-16 bg-white">
+
+      <div class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1">
+
+
+        <a :href="url" class="flex items-center h-full text-xl">{{
+        company
+        }}</a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "MainNav",
+  data() {
+    return {
+      company: "Google Job Portal",
+      url: "https://careers.google.com",
+      menuItems: ["Teams", "Locations", "Life at Google", "How we hire", "Jobs"]
+    };
+  },
+};
+</script>
